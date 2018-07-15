3a7befa753ff67f41adf4aadebfbf83522d01288
praveen-vp <praveen_v.p@outlook.com>
2018-07-15 13:19:22 +0530

    Adding project files

---
+++ src/main/java/com/pvp/joke/springgurujokeapp/SpringGuruJokeAppApplication.java
@@ -0,0 +1,12 @@
+package com.pvp.joke.springgurujokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuruJokeAppApplication.class, args);
+	}
+}
